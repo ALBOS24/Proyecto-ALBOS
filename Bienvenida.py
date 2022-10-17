9ca202b178563552064d6ecaa64a877d23792d0f
CSgalvezE <112191679+CSgalvezE@users.noreply.github.com>
2022-10-16 21:11:58 -0600

    Primera sección de linea de cogido.

---
+++ Bienvenida.py
@@ -0,0 +1,5 @@
+# En la carpeta llamada "Lineas de codigo" guardaremos de forma seccionada nuestro pequeño proyecto. 
+# Comenzaremos con el mensaje de Bienvenida de ALBOS. 
+nombre= input("Cual es tu nombre?")
+print("Hola "+ nombre+ " Bienvenido al proyecto ALBOS")
+print("Aquí encontraras la informacion de riego sobre 3 cultivos de interes: Maíz, Sorgo y Frijol")
